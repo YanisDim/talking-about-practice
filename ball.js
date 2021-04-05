30731cfbb053865751fd5082757fb6a380555413
Yanis Dimitropoulos <yanis.dim@gmail.com>
2021-04-05 14:30:14 +0200

    ball thrown

--- ball.js
+++ ball.js
@@ -1,12 +1,8 @@
 class Ball{
-    constructor(x, y, vx, vy ){
+    constructor(x, y, incrBall ){
         this.x = x;
         this.y = y;
-        this.velox = vx;
-        this.veloy = vy;
-        this.nextBall= false;
-        this.ballReceived = false; 
-        this.ballWidth = 50;
-        this.ballHeight = 50;
+        this.incrBall = 1;
+        
     }
 }
--- main.js
+++ main.js
@@ -36,24 +36,44 @@ ball.src = './Img/ball.png'
 //---------------------
 //Variables creation
 let userX = 260, userY= canvas.height-270, userIncr= 5, userWidth=150, userHeight = 150
-let cpuX = 260,  cpuWidth = 150, cpuHeight=150, cpuY = 70
+let cpuX = 260,  cpuWidth = 150, cpuHeight=150, cpuY = 70;
+let ballHeight = 50, ballWidth = 50
 let isArrowLeft = false, isArrowRight = false;
 let incrX = 5;//increment x position of cpu
 let isGameOver = false;
 let intervalId = 0
 let score = 0
-let randomCpuX = cpuX * Math.random()
-let balls = []
+//let balls = []
+let myBall = new Ball (cpuX, cpuY)
+let nextBall = {}
+
+
+
 
 //---------------------
-/*
-// creation of the balls
+//creation of the balls
 function passTheBall(){
-    randomCpuX
-    let ballIncrY = cpuY + Math.random(1)
-    balls.push(new Ball(cpuX, cpuY, ballIncrY))
-}
-*/
+    ctx.drawImage(ball, myBall.x, myBall.y, ballWidth, ballHeight)
+    myBall.y = myBall.y + myBall.incrBall
+    let staticBall = new Ball(cpuX, cpuY)
+    ctx.drawImage(ball, staticBall.x + (cpuWidth/2), staticBall.y+(cpuHeight/2), ballWidth, ballHeight)
+
+    if (myBall.y == userY){
+        console.log(myBall.y == userY)
+        nextBall = new Ball (cpuX, cpuY)
+        
+        
+    
+    }if (nextBall.y){
+        ctx.drawImage(ball, nextBall.x, nextBall.y, ballWidth, ballHeight)
+        nextBall.y = nextBall.y + nextBall.incrBall
+        
+    } if (nextBall.y == userY){
+        myBall = new Ball (cpuX, cpuY)
+    }
+    //balls.push(new Ball(ball.x, ball.y, ball.incrBall))    
+    }
+    
 
 
 //add background
@@ -61,9 +81,8 @@ function start(){
     startPage.style.display = 'none'
         gamePage.style.display = ''
         draw()
-        animate()
-       // passTheBall()
-        
+        animate() 
+         
 }
 
 function restart (){
@@ -103,6 +122,8 @@ function animate(){
 //auto animation cpu
 ctx.clearRect(0, 0, canvas.width, canvas.height)
 draw()
+passTheBall()
+
 //score
 ctx.fillStyle = 'brown'
 ctx.font = '24px Kaushan Script'
@@ -159,8 +180,7 @@ window.addEventListener('load', ()=>{
     startBtn.addEventListener('click', ()=>{
         start()
     })
-    gameOverPage.style.display('click', ()=>{
+    /*gameOverPage.style.display('click', ()=>{
         restart()
-    })
+    })*/
 })
-
