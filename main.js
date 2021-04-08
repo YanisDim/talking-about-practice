14b0b2cda451c915185fc5e18b19e9feb1322aee
Yanis Dimitropoulos <yanis.dim@gmail.com>
2021-04-09 01:31:39 +0200

    bug fix

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ ball.src = './Img/ball.png'
 let gator = new Image();
 gator.src = './Img/gatorade.png'
 
-let deady = new Image();
-deady.src = './Img/deady.png'
+//let deady = new Image();
+//deady.src = './Img/deady.png'
 
 //ball touch sounds
 let audio = new Audio()
@@ -188,24 +188,24 @@ function selectPlayer(){
        else if (document.querySelector('#btn-radio3').checked){
            user.src='./Img/player3.png'
        } else{
-           user.src = '/Img/Marvel characters/deady.png'
+           user.src = '/Img/deady.png'
        }
     
 }
 
 function marvelTransformation(){
-    if (score > 40)user.src = ('/Img/Marvel characters/ironman.png')
-    if (score > 60) user.src = ('/Img/Marvel characters/venom.png')
-    if (score > 80) user.src = ('/Img/Marvel characters/blackwidow-removebg-preview.png')
-    if (score > 100) user.src = ('/Img/Marvel characters/wolvy-removebg-preview.png')
-    if (score > 120) user.src = ('/Img/Marvel characters/hulk-removebg-preview.png')
-    if (score > 140) user.src = ('/Img/Marvel characters/storm-removebg-preview.png')
-    if (score > 180) user.src = ('/Img/Marvel characters/spiderman-removebg-preview.png')
-    if (score > 220) user.src = ('/Img/Marvel characters/blackpanther.png')
-    if (score > 260) user.src = ('/Img/Marvel characters/captainamerica-removebg-preview.png')
-    if (score > 300) user.src= ('/Img/Marvel characters/deady.png')
-    if (score > 320) user.src = ('/Img/Marvel characters/jordan-removebg-preview.png')
-    if (score > 400) user.src = ('/Img/Marvel characters/png-clipart-goat-drawing-chibi-art-goat-purple-animals-removebg-preview.png')
+    if (score > 40)user.src = ('/Img/ironman.png')
+    if (score > 60) user.src = ('/Img/spiderman.png')
+    if (score > 80) user.src = ('/Img/storm.png')
+    if (score > 100) user.src = ('/Img/wolvy.png')
+    if (score > 120) user.src = ('/Img/blackpanther.png')
+    if (score > 140) user.src = ('/Img/blackwidow.png')
+    if (score > 180) user.src = ('/Img/captainamerica.png')
+    if (score > 220) user.src = ('/Img/captainmarvel.png')
+    if (score > 260) user.src = ('/Img/hulk.png')
+    if (score > 300) user.src= ('/Img/deady.png')
+    if (score > 320) user.src = ('/Img/jordan.png')
+    if (score > 400) user.src = ('/Img/goat.png')
 }
 
 
