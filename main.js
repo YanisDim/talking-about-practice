179debf63e8e287be814adde2507e852f875854b
Yanis Dimitropoulos <yanis.dim@gmail.com>
2021-04-09 03:07:57 +0200

    html fix

--- main.js
+++ main.js
@@ -2,8 +2,6 @@
 let canvas = document.querySelector('#myCanvas');
 let ctx = canvas.getContext('2d');
 canvas.style.border = '5px solid white';
-
-
 //the DOM pages selectors
 let startPage = document.querySelector('#start-page')
 let gamePage = document.querySelector('#game-page')
@@ -12,33 +10,19 @@ let gameOverPage = document.querySelector('#game-over-page')
 let startBtn = document.querySelector('#start-btn')
 let restartBtn = document.querySelector('#restart')
 let finalscore = document.querySelector('#final-score')
-//-------------------
-//load all images
-
-//background image
+//load images
 let court = new Image();
 court.src = './Img/halfcourt.jpg';
-
-
 let user = new Image();
-
-
-
 let cpu = new Image();
 cpu.src = './Img/player4.png';
-
 let ball = new Image();
 ball.src = './Img/ball.png'
-
 let gator = new Image();
 gator.src = './Img/gatorade.png'
-
-
-
-//ball touch sounds
+//sounds
 let audio = new Audio()
 audio.src = './sounds/mixkit-catching-a-basketball-ball-2081.wav'
-
 let audioBounce = new Audio()
 audioBounce.src= './sounds/mixkit-basketball-ball-hard-hit-2093.wav'
 let audioMainPage = new Audio ()
@@ -49,107 +33,72 @@ audioGameOver.src = './sounds/Full Crate - Pump Up The Jam [Remix].mp3'
 audioGameOver.volume= 0.1
 let audioSip = new Audio ()
 audioSip.src= './sounds/152363__cogitoandcradle__the-sound-someone-makes-after-take-n-a-drink-of-delicious-milk.wav'
-
-//---------------------
 //Variables creation
-let userX = 260, userY= canvas.height-270, userIncr= 5, userWidth=150, userHeight = 150
-let cpuX = 260,  cpuWidth = 150, cpuHeight=150, cpuY = 70; 
+let userX = 260, userY= canvas.height-270, userIncr= 5, userWidth=150, userHeight = 150 //User is our character
+let cpuX = 260,  cpuWidth = 150, cpuHeight=150, cpuY = 70; //CPU is the compputer player
 let ballHeight = 50, ballWidth = 50
 let isArrowLeft = false, isArrowRight = false;
-let incrX = 5;//increment x position of cpu
+let incrX = 5;
 let isGameOver = false;
 let intervalId = 0
 let score = 0
-//let balls = []
 let myBall = new Ball (cpuX+(cpuWidth/2), cpuY+(cpuHeight/2))
 let nextBall = {}
-
-//energy variables
 let maxEnergy = 5;
-let gatorBottle;
+let gatorBottle;// gator is short for gatorade
 let gatorWidth = 46;
 let bottleScore = 5;
 
-
-
-
-
-//---------------------
-//creation of the balls
+//creation of the balls and movement (here three balls are created in total)
 function passTheBall(){
     ctx.drawImage(ball, myBall.x, myBall.y, ballWidth, ballHeight)
     myBall.y = myBall.y + myBall.incrBall
     let staticBall = new Ball(cpuX+(cpuWidth/2), cpuY+(cpuHeight/2))
     ctx.drawImage(ball, staticBall.x , staticBall.y, ballWidth, ballHeight)
-    
     if (myBall.y >= userY && myBall.y < userY+10){
         nextBall = new Ball (cpuX+(cpuWidth/2), cpuY+(cpuHeight/2))
-        
-        
-    
-    }if (nextBall.y){
+    }
+    if (nextBall.y){
         ctx.drawImage(ball, nextBall.x , nextBall.y, ballWidth, ballHeight)
-        nextBall.y = nextBall.y + nextBall.incrBall
-        
-    } if (nextBall.y >= userY && nextBall.y< userY+10){
-        console.log('myBall is creatd')
-        myBall = new Ball (cpuX+(cpuWidth/2), cpuY+(cpuHeight/2))
-       
+        nextBall.y = nextBall.y + nextBall.incrBall 
+    } 
+    if (nextBall.y >= userY && nextBall.y< userY+10){
+        myBall = new Ball (cpuX+(cpuWidth/2), cpuY+(cpuHeight/2)) 
     }
-      
     }
 
-
-    function collision(){
-        
-        
-        if (myBall.y >= userY+70 && myBall.y<=userY+75 && (myBall.x >= userX )&& (myBall.x <= userX+userWidth  ) ){
+// collision between the balls and the player to increment score
+function collision(){
+    if (myBall.y >= userY+70 && myBall.y<=userY+75 && (myBall.x >= userX )&& (myBall.x <= userX+userWidth  ) ){
             score ++
             audio.play()
-            
             myBall.y = canvas.height + ballHeight
-           
          } 
          if (nextBall.y >= userY+70 && nextBall.y <=userY+75  && nextBall.x >userX && nextBall.x < userX+userWidth ){
              score++
              audio.play()
              nextBall.y = canvas.height + ballHeight
-
-         }
-
-            
-        
-
-         
+         }    
     }
 
-    
-    
-
-
-//add background
+//start function displaying the game page
 function start(){
     startPage.style.display = 'none'
     audioMainPage.pause()
-        gamePage.style.display = ''
-        selectPlayer()
-        draw()
-        animate() 
-        
-        
+    gamePage.style.display = ''
+    selectPlayer()
+    draw()
+    animate()        
 }
 
-function energy(){
-    
-    // condition. if (score is modulus 5. create new bottle)
+function energyTracking(){
+// condition. if (score is modulus 5. create new bottle)
     if (score >= bottleScore && maxEnergy < 5 && !gatorBottle){
         gatorBottle = new Gator
     }
-    if (gatorBottle) {
+    if (gatorBottle){
         ctx.drawImage(gator,gatorBottle.gatorx, gatorBottle.gatory)
     }
-
-    
     //decreasing energy
     if (maxEnergy <= 5 && maxEnergy > 0 && myBall.y > canvas.height && myBall.y < canvas.height + 10) maxEnergy--
     if (maxEnergy <= 5 && maxEnergy > 0 && nextBall.y > canvas.height && nextBall.y < canvas.height + 10) maxEnergy--
@@ -159,18 +108,9 @@ function energy(){
             audioSip.play()
             gatorBottle = null
             bottleScore = score + 5
-            
-            
         }
-        
-
-    
+//GameOverCondition
     if (maxEnergy == 0) isGameOver = true
-
-    
-    
-    
-
 }
 
 function selectPlayer(){
@@ -184,18 +124,12 @@ function selectPlayer(){
        } else{
            user.src='./Img/deady.png'
        }
-    
 }
 
-
 function draw(){
-ctx.drawImage(court, 0, 0)
-    //adding user player
-    
-ctx.drawImage(user, userX, userY, userWidth, userHeight)//530 moving line
-//adding computer player
-ctx.drawImage(cpu, cpuX, cpuY, cpuWidth, cpuHeight) //70 moving line
-
+    ctx.drawImage(court, 0, 0)
+    ctx.drawImage(user, userX, userY, userWidth, userHeight)
+    ctx.drawImage(cpu, cpuX, cpuY, cpuWidth, cpuHeight)
 }
 
 function marvelTransformation(){
@@ -234,14 +168,12 @@ function marvelTransformation(){
     } 
 }
 
-
 //Keyboard use
 document.addEventListener('keydown', (event)=>{
     if(event.code == 'ArrowRight'){
         isArrowRight = true
         isArrowLeft = false
-    }
-    else if (event.code =='ArrowLeft'){
+    } else if (event.code =='ArrowLeft'){
         isArrowRight = false
         isArrowLeft = true
     }
@@ -252,44 +184,35 @@ document.addEventListener('keyup', (event)=>{
     isArrowLeft = false
 })
 
-
-
 function animate(){
-ctx.clearRect(0, 0, canvas.width, canvas.height)
-draw()
-energy()
-passTheBall()
-collision()
-marvelTransformation()
-
-
-ctx.fillStyle = 'brown'
-ctx.font = '24px Kaushan Script'
-ctx.fillText(`Score is : ${score}`, 20, 30)
-ctx.fillText(`Energy Level : ${maxEnergy}`, 20, 60)
-//Right hand side 
-if (cpuX > canvas.width - 120 ){
-    incrX = -incrX
-}
-
-//Left hand
-if(cpuX + 30 < 0){
-    incrX = 5
-}
-cpuX = cpuX + incrX
-
-
-
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    draw()
+    energyTracking()
+    passTheBall()
+    collision()
+    marvelTransformation()
+    ctx.fillStyle = 'brown'
+    ctx.font = '24px Kaushan Script'
+    ctx.fillText(`Score is : ${score}`, 20, 30)
+    ctx.fillText(`Energy Level : ${maxEnergy}`, 20, 60)
+//checking right hand side of the computer player
+    if (cpuX > canvas.width - 120 ){
+        incrX = -incrX
+    }
+//Left hand side checking
+    if(cpuX + 30 < 0){
+        incrX = 5
+    }
+    cpuX = cpuX + incrX
 //animate userPlayer
-if(isArrowRight && userX + userWidth < canvas.width +40){
-    userX = userX + 5
-}
-if(isArrowLeft && userX + 40 > 0){
-    userX = userX - 5
-}
+    if(isArrowRight && userX + userWidth < canvas.width +40){
+        userX = userX + 5
+    }
+    if(isArrowLeft && userX + 40 > 0){
+        userX = userX - 5
+    }
 
-//game over condition
+//game over setup
 if (isGameOver){
     cancelAnimationFrame(intervalId)
     gameOverPage.style.display = 'block'
@@ -300,9 +223,6 @@ if (isGameOver){
 } else{
     intervalId = requestAnimationFrame(animate)
 }
-
-
-
 }
 
 function restart(){
@@ -315,19 +235,6 @@ function restart(){
     maxEnergy = 5
     start()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 window.addEventListener('load', ()=>{
     startPage.style.display = 'block'
